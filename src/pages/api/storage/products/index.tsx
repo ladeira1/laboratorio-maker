c45ce4cdb67f50e4bc056bf1cf466842c955dffc
Joao Ladeira <joaoladeirag@gmail.com>
2022-04-03 11:24:08 -0300

    Remove logs

--- src/pages/api/storage/products/index.tsx
+++ src/pages/api/storage/products/index.tsx
@@ -2,14 +2,11 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "services/prisma";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-  let a = "";
   try {
     if (req.method === "GET") {
       const page = Number(req.query.page);
       const limit = Number(req.query.limit);
       const startIndex = (page - 1) * limit;
-
-      a += "b";
 
       const products = await prisma.product.findMany({
         orderBy: { updatedAt: "asc" },
@@ -18,18 +15,15 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         skip: startIndex,
       });
 
-      a += "c";
       const totalPages = Math.ceil((await prisma.product.count()) / limit);
       const nextPage = page + 1 > totalPages ? null : page + 1;
-
-      a += "d";
 
       return res.status(201).json({ products, nextPage, totalPages });
     } else {
       return res.status(400).json({ error: "Invalid request" });
     }
   } catch (error) {
-    return res.status(400).json({ error, a });
+    return res.status(400).json({ error });
   }
 };
 
