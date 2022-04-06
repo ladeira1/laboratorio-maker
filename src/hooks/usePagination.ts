f885b464d3036a642336a5fc553c493cdf9697c1
Joao Ladeira <joaoladeirag@gmail.com>
2022-04-06 17:36:08 -0300

    Fix usePagination indent

--- src/hooks/usePagination.ts
+++ src/hooks/usePagination.ts
@@ -6,33 +6,34 @@ interface UsePaginationProps {
   request: () => Promise<AxiosResponse<Record<string, any>, any>>;
 }
 
-  export const usePagination = ({ request }: UsePaginationProps) => {
+export const usePagination = ({ request }: UsePaginationProps) => {
   const { error: errorToast } = useStyledToast();
 
-    const [page, setPage] = useState<number | null>(1);
-    const [totalPages, setTotalPages] = useState<number>(2);
-    const [isLoading, setIsLoading] = useState(false);
-    const [hasError, setHasError] = useState<boolean>(false)
+  const [page, setPage] = useState<number | null>(1);
+  const [totalPages, setTotalPages] = useState<number>(2);
+  const [isLoading, setIsLoading] = useState(false);
+  const [hasError, setHasError] = useState<boolean>(false);
 
-    const shouldFetchMoreData = !hasError && page !== null && totalPages >= page && !isLoading
+  const shouldFetchMoreData =
+    !hasError && page !== null && totalPages >= page && !isLoading;
 
-    const fetchMoreData = async () => {
-      try{
-        setHasError(false);
-        setIsLoading(true);
-        const response = await request();
-  
-        setPage(response.data.nextPage);
-        setTotalPages(response.data.totalPages);
-        setIsLoading(false);
+  const fetchMoreData = async () => {
+    try {
+      setHasError(false);
+      setIsLoading(true);
+      const response = await request();
 
-        return response.data;
-      } catch(err) {
-        setIsLoading(false)
-        setHasError(true)
-        errorToast({ error: err });
-      }
+      setPage(response.data.nextPage);
+      setTotalPages(response.data.totalPages);
+      setIsLoading(false);
+
+      return response.data;
+    } catch (err) {
+      setIsLoading(false);
+      setHasError(true);
+      errorToast({ error: err });
     }
+  };
 
   return {
     page,
