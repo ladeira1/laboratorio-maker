9f6e824c8e925cf5c3d5cba721a42a37d967d034
Joao Ladeira <joaoladeirag@gmail.com>
2022-04-06 17:21:18 -0300

    Add Validator when creating a product

---
+++ src/validators/storage/products/create.ts
@@ -0,0 +1,47 @@
+import { NextApiRequest } from "next";
+import { prisma } from "services/prisma";
+import * as yup from "yup";
+
+const schema = yup.object({
+  name: yup.string().required("Campo obrigatório"),
+  amount: yup
+    .number()
+    .typeError("Você precisa informar um número")
+    .integer("A quantidade informada precisa ser um número inteiro")
+    .required("Campo obrigatório"),
+});
+
+export const createProductValidator = async (req: NextApiRequest) => {
+  const { name, door, floor, categoryId, lockerId } = req.body;
+
+  if (!(await schema.isValid(req.body))) {
+    await schema
+      .validate(req.body, {
+        abortEarly: true,
+      })
+      .catch((err) => {
+        throw new Error(err);
+      });
+  }
+
+  const product = await prisma.product.findFirst({
+    where: { name, categoryId },
+  });
+  if (product) throw new Error("Este produto já existe");
+
+  const category = await prisma.category.findFirst({
+    where: { id: categoryId },
+  });
+  if (!category) throw new Error("Categoria não encontrada");
+
+  const locker = await prisma.locker.findFirst({ where: { id: lockerId } });
+  if (!locker) throw new Error("Armário não encontrado");
+
+  if (locker.numberOfDoors < door) {
+    throw new Error("A porta informada não existe");
+  }
+
+  if (locker.numberOfFloors < floor) {
+    throw new Error("O andar informado não existe");
+  }
+};
